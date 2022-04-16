86f9d4e604f5e794a333e59817e62ed69111d232
Liku cs <83144124+likucs@users.noreply.github.com>
2022-04-16 22:13:06 +0530

    Update login.py

--- plugins/login.py
+++ plugins/login.py
@@ -43,21 +43,17 @@ async def login(bot, message):
             HOME_TEXT.format(message.from_user.first_name, message.from_user.id, USER, USER, USER, OWNER), 
 			disable_web_page_preview=True,
 			reply_markup=InlineKeyboardMarkup(
+			[
 				[
-					[
-						InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-						InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122")
-					],
-                    [
-                        InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
-						InlineKeyboardButton("🧩Deploy Own Bot", url="https://heroku.com/deploy?template=https://github.com/subinps/Instagram-Bot")
-                    ],
-                    [
-                        InlineKeyboardButton("👨🏼‍🦯How To Use?", callback_data="help#subin")
-
-                    ]
-					
+					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/iAmLiKu1'),
+					InlineKeyboardButton("🤖Other Bots", url="https://t.me/cs_cloud"),
+                    InlineKeyboardButton("⚙️Update Channel", url="https://t.me/cs_cloud")
+				],
+				[
+					InlineKeyboardButton("🔗Source Code", url="https://t.me/cs_cloud"),
+					InlineKeyboardButton("Plx don't block me", url="https://t.me/INSTGRAM_DL_CS_BOT")
 				]
+			]
 			)
 		)
         return
@@ -171,21 +167,17 @@ async def logout(bot, message):
             HOME_TEXT.format(message.from_user.first_name, message.from_user.id, USER, USER, USER, OWNER), 
 			disable_web_page_preview=True,
 			reply_markup=InlineKeyboardMarkup(
+			[
 				[
-					[
-						InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-						InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122")
-					],
-                    [
-                        InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
-						InlineKeyboardButton("🧩Deploy Own Bot", url="https://heroku.com/deploy?template=https://github.com/subinps/Instagram-Bot")
-                    ],
-                    [
-                        InlineKeyboardButton("👨🏼‍🦯How To Use?", callback_data="help#subin")
-
-                    ]
-					
+					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/iAmLiKu1'),
+					InlineKeyboardButton("🤖Other Bots", url="https://t.me/cs_cloud"),
+                    InlineKeyboardButton("⚙️Update Channel", url="https://t.me/cs_cloud")
+				],
+				[
+					InlineKeyboardButton("🔗Source Code", url="https://t.me/cs_cloud"),
+					InlineKeyboardButton("Plx don't block me", url="https://t.me/INSTGRAM_DL_CS_BOT")
 				]
+			]
 			)
 		)
         return
